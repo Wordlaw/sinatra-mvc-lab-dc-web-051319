148fcc746b914a0a1543fa110c9b128f99adb10a
github username <github email address>
2019-06-06 16:59:29 -0400

    semi-working

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+  get '/user_input' do
+    erb :user_input
+  end
+
+  get '/user_input/piglatin' do
+    erb :piglatin
+  end
+
+  post '/user_input' do
+    vowels = ["a", "e", "i", "o", "u"]
+    @phrase = params[:phrase]
+
+    erb :user_input
+    redirect('/user_input/piglatin')
+  end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,19 @@
+#require_relative 'config/environment'
+
+# class Piglatinizer
+#   attr_accessor :phrase
+#   @phrase
+#
+#
+#
+#   def piglatin
+#
+#
+#   end
+#
+#   def drop_firsts
+#     vowels = ["a", "e", "i", "o", "u"]
+#   end
+#
+#
+# end
